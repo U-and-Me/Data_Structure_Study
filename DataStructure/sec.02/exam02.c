89e4c03e4f339af56da706bfc0e3fb66b1d85e40
Kim Yu Na <yunasun0920@gmail.com>
2022-02-04 19:55:37 +0900

    C 기초 문법

---
+++ DataStructure/sec.02/exam02.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define BUFFER_SIZE 100
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char* words[100];
+	char buffer[BUFFER_SIZE];
+
+	int word_cnt = 0;
+
+	while (word_cnt < 4 && scanf("%s", buffer) != EOF) {
+		words[word_cnt] = strdup(buffer);
+		word_cnt++;
+	}
+
+	return 0;
+}
